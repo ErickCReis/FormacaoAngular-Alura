741f87ecb3dab5f4e8019735ff89d6c33a933723
ErickCReis <erickcorreareis@gmail.com>
2021-12-15 10:09:34 -0300

    Boas práticas em arquiteturas e formulários: Aula 2 - Login

--- Boas práticas em arquitetura e formulários/gatitobook/src/app/home/login/login.component.ts
+++ Boas práticas em arquitetura e formulários/gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(
+    private authService: AutenticacaoService,
+    private route: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe({
+      next: () => {
+        this.route.navigate(['animais']);
+        console.log('Usuário autenticado com sucesso')
+      },
+      error: (erro) => {
+        alert('Falha ao autenticar usuário');
+        console.error(erro);
+      },
+    });
   }
-
 }
