ad2d09577c4b6931f396abb5588319ea7336f27f
ErickCReis <erickcorreareis@gmail.com>
2022-01-24 16:29:55 -0300

    Avançando com testes automatizados: Aula 2 - Testes temporais e de integração com o DOM

--- Avançando com testes automatizados/README.md
+++ Avançando com testes automatizados/README.md
@@ -7,3 +7,10 @@
 - Criação do component PhotoFrame
 - Aplicação do pattern debounce
 
+### Aula 2 - Testes temporais e de integração com o DOM
+
+- O papel da função fakeAsync
+- Controle de tempo com a função tick
+- Teste de integração com o DOM
+- Teste de atributos
+- Testando a acessibilidade
--- Avançando com testes automatizados/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ Avançando com testes automatizados/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
 import { PhotoFrameModule } from './photo-frame.module';
 
@@ -17,5 +22,66 @@ describe(PhotoFrameComponent.name, () => {
 
   it('Should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+   should trigger (@Output liked) once called
+   multiple times within dedbouce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    component.like();
+    tick(500);
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+   should trigger (@Output liked) two times when called
+   outside debouce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
+    expect(times).toBe(2);
+  }));
+
+  it(`(D) Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement =
+      fixture.nativeElement.querySelector('.like-counter');
+    expect(element.textContent.trim()).toBe('1');
+  });
+
+  it(`(D) Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement =
+      fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`(D) Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
+
+  it(`(D) Should display with img and description when bound to property`, () => {
+    const src = 'https://picsum.photos/200/300';
+    const description = 'This is a description';
+    component.src = src;
+    component.description = description;
+    fixture.detectChanges();
+    const element: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    expect(element.getAttribute('src')).toBe(src);
+    expect(element.getAttribute('alt')).toBe(description);
   });
 });
